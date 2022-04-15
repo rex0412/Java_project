30471db94351eb4e54a82ba82f64d990a8f6ef9a
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-04-15 17:32:10 +0900

    생성자의 오버로딩, 객체 생성 시 생성자 선택

--- Car.java
+++ Car.java
@@ -1,10 +1,34 @@
 /*Car 클래스 필드 선언*/
 public class Car {
-	//필드
+	/* //필드
 	String company = "현대자동차";
 	String model = "그랜저";
 	String color = "검정";
 	int maxSpeed = 350;
-	int speed;
-
+	int speed; */
+	
+	//필드
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
+	
+	//생성자
+	Car(){
+}
+	
+	Car(String model) {
+		this.model = model;
+	}
+	
+	Car(String model, String color) {
+		this.model = model;
+		this.color = color;
+	}
+	
+	Car(String moder, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
 }
--- CarExample.java
+++ CarExample.java
@@ -1,8 +1,9 @@
+
 /*외부 클래스에서 Car 필드랎 읽기와 변경*/
 public class CarExample {
 
 	public static void main(String[] args) {
-		//객체 생성
+		/* //객체 생성
 		Car myCar = new Car();
 		
 		//필드값 읽기
@@ -15,6 +16,28 @@ public class CarExample {
 		//필드값 변경
 		myCar.speed = 60;
 		System.out.println("수정된 속도: " + myCar.speed);
-	}
+	} */
+		
+		Car car1 = new Car();
+		System.out.println("car1.company : " + car1.company);
+		System.out.println();
+		
+		Car car2 = new Car("자가용");
+		System.out.println("car2.company : " + car2.company);
+		System.out.println("car2.model : " + car2.model);
+		System.out.println();
+		
+		Car car3 = new Car("자가용", "빨강");
+		System.out.println("car3.company : " + car3.company);
+		System.out.println("car3.model : " + car3.model);
+		System.out.println("car3.color : " + car3.color);
+		System.out.println();
+		
+		Car car4 = new Car("택시", "검정", 200);
+		System.out.println("car4.company : " + car4.company);
+		System.out.println("car4.model : " + car4.model);
+		System.out.println("car4.color : " + car4.color);
+		System.out.println("car4.maxSpeed : " + car4.maxSpeed);
 
+	}	
 }
