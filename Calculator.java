6b4e78aa7c85919c68d0865ddb5f4d324ce10afb
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2022-04-19 17:18:24 +0900

    메소드 오버로딩

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+/*메소드 오버로딩*/
+public class Calculator {
+	//정사각형 넓이
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	//직사각형의 넓이
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+
+}
